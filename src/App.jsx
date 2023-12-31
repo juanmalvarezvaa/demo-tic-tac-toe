22e4c7fc371e87da1fdf18d564f6417294d3c9e5
Juan <juan.alvarez.vaa@gmail.com>
2023-12-31 11:03:49 -0300

    Add base Player component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import Player from "./components/Player";
+
 const App = () => {
-  return <p>TODO</p>;
+  return (
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Player name="Player 1" symbol="X" />
+          <Player name="Player 2" symbol="O" />
+        </ol>
+        GAMEBOARD
+      </div>
+      LOG OF PLAYS
+    </main>
+  );
 };
 
 export default App;
