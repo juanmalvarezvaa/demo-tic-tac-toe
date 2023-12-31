281244f87697d175ec87bd27e9523a942141a31f
Juan <juan.alvarez.vaa@gmail.com>
2023-12-31 11:53:39 -0300

    Add static gameboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Player from "./components/Player";
+import GameBoard from "./components/GameBoard";
 
 const App = () => {
   return (
@@ -8,7 +9,7 @@ const App = () => {
           <Player name="Player 1" symbol="X" />
           <Player name="Player 2" symbol="O" />
         </ol>
-        GAMEBOARD
+        <GameBoard />
       </div>
       LOG OF PLAYS
     </main>
