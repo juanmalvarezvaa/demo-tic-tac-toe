6d096dbaea3e2b5214c426dd57526eec4266f16d
Juan <juan.alvarez.vaa@gmail.com>
2023-12-31 11:25:57 -0300

    Player name editable

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,15 +1,35 @@
 import { useState } from "react";
 
-export default Player = ({ name, symbol }) => {
-//   const [isEditing, setIsEditing] = useState();
+const Player = ({ name, symbol }) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [playerName, setPlayerName] = useState(name);
+
+  const handleEditClick = () => {
+    setIsEditing((edit) => !edit);
+  };
+
+  const handleNameChange = (event) => {
+    setPlayerName(event.target.value);
+  };
 
   return (
     <li>
       <span className="player">
-        <span className="player-name">{name}</span>
+        {isEditing ? (
+          <input
+            type="text"
+            required
+            value={playerName}
+            onChange={handleNameChange}
+          />
+        ) : (
+          <span className="player-name">{playerName}</span>
+        )}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button>Edit</button>
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
     </li>
   );
 };
+
+export default Player;
